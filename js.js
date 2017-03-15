1ebb376f6fd8c8f58c99af88e53a0f09108bf525
Acircle <upupwangxianxi@163.com>
2017-03-15 09:28:40 +0800

    Update js.js
    简单 获得 url  中的参数

--- js.js
+++ js.js
@@ -4,3 +4,10 @@ var Obj = (function () {
 		getname:this.name
 	}
 })()
+
+
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
